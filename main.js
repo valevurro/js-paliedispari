e66654b8697300571d5ec63c59338f1248455357
valevurro <vale.v.00@outlook.it>
2025-07-20 15:58:58 +0200

    Add changes

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 // Chiediamo una parola all'utente
-const parolaUtente = prompt("Inserisci una parola");
+const word = prompt("Inserisci una parola");
 
 // Funzione per verificare se la parola è palindroma
-function èPalindroma(parola) {
-    const parolaInversa = parola.split('').reverse().join('');
-    return parola === parolaInversa;
+function reverseWord(word) {
+    let reversed = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        const singolo_carattere = word[i];
+        reversed += singolo_carattere;
+    }
+
+  return reversed;
 }
 
-// Verifica e stampa il risultato
-if (èPalindroma(parolaUtente)) {
-    console.log("La parola è palindroma!");
-} else {
-    console.log("La parola NON è palindroma.");
+//funzione con return true o false
+function isPalindrome(word) {
+    const reversedWord = reverseWord(word);
+    console.log(reversedWord);
+  
+    if (word === reversedWord) {
+      return true;
+    } else {
+      return false;
+    }
 }
+  
+  //output palindroma o non palindroma
+if (isPalindrome(word)) {
+    console.log(`${word} è palindroma`);
+} else {
+    console.log(`${word} non è palindroma`);
+}
+
 
 
